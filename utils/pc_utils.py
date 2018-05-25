717fdebf9180ddaede0df4ae476d28cfaf66dea9
vdutell <vashadutell@gmail.com>
2018-05-24 22:32:45 -0700

    ensure singal is real

--- utils/pc_utils.py
+++ utils/pc_utils.py
@@ -20,23 +20,53 @@ def measure_pc_axis(img, axis):
     pc = e/np.mean(np.abs(np.fft.fft(img,axis=axis)))
     return(pc)
 
-#measure energy over an image
-def measure_energy_2d(f):
+def measure_energy1_2d(f):
     
-    #x_e = measure_energy_axis(img,axis=ss0)
-    #y_e = measure_energy_axis(img,axis=1)
-    #energy2d = np.sum([x_e,y_e],axis=0)
+    hil = hilbert2(f)
+    energy2d = np.sqrt(np.square(np.real(hil))+np.square(np.imag(hil)))
+    #phibar = np.arctan(h/f)
+    phibar = np.arctan2(np.imag(hil),np.real(hil))
     
-    energy2d = np.sqrt(np.square(f)+np.square(np.imag(hilbert2(f))))
-    
-    return(energy2d)
+    return(energy2d, phibar)
 
-#measure phase congruency
-def measure_pc_2d(img, epsilon = 0.01):
+
+def measure_pc1_2d(img, epsilon=0.001):
+
+    a = np.abs(np.fft.fft2(img))+epsilon
+    e, phibar = measure_energy1_2d(img)
+    pc_vals = np.divide(e,np.mean(a))
+
+    return(pc_vals, phibar)
     
-    #can't just divide x and y individually.
-    e_vals = measure_energy_2d(img)
-    a = np.mean(np.abs(np.fft.fft2(img)))+epsilon
-    pc_vals = np.divide(e_vals, a)
+def measure_energy2_2d(f):
+
+    h = np.imag(hilbert2(f))
+    #phibar = np.arctan(h/f)
+    phibar = np.arctan2(h,f)
+    energy2d = np.multiply((np.cos(phibar) - np.abs(np.sin(phibar))), f) 
+
+    return(energy2d, phibar)
+
     
-    return(pc_vals)
+def measure_pc2_2d(img, epsilon=0.001):
+
+    a = np.abs(np.fft.fft2(img))+epsilon
+    e, phibar = measure_energy2_2d(img)
+    pc_vals = np.divide(e,np.sum(a))
+    
+    return(pc_vals, phibar)
+
+    
+def measure_pc_2d(img, pcn = 1, pbflag=True, epsilon= 0.001):
+    # wrapper function to take care of two different pc measurement types
+    
+    if(pcn==1):
+        pc_vals, phibar = measure_pc1_2d(img, epsilon)
+    elif(pcn==2):
+        pc_vals, phibar = measure_pc2_2d(img, epsilon)
+
+    if(pbflag==False):
+        return(pc_vals)
+    else:
+        return(pc_vals, phibar)
+        
